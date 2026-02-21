2e7479c21e335e630e78ac8a68411086ebbbe2ce
AiruxPH <randythegreat000@gmail.com>
2026-02-21 13:52:56 +0800

    feat: Add `CertificationsSection` custom element to display professional certifications.

--- js/components/CertificationsSection.js
+++ js/components/CertificationsSection.js
@@ -12,7 +12,7 @@ class CertificationsSection extends HTMLElement {
                 <div class="projects-grid">
                      <div class="project-card glass-panel" style="align-items: center; text-align: center;">
                         <img src="images/CCNA - Introduction to Networks.png" alt="CCNA Badge" style="max-width: 200px; margin-bottom: 15px;">
-                        <h3><a href="https://www.credly.com/badges/6779285f-4386-4978-877a-75171593580b/" target="_blank" style="color: white; text-decoration: none;">CCNA: Introduction to Networks <i class="fa-solid fa-external-link-alt" style="font-size: 0.8em; margin-left: 5px; color: var(--neon-cyan);"></i></a></h3>
+                        <h3><a href="https://www.credly.com/badges/c384d9a2-979a-4215-925a-997620c05ac4" target="_blank" style="color: white; text-decoration: none;">CCNA: Introduction to Networks <i class="fa-solid fa-external-link-alt" style="font-size: 0.8em; margin-left: 5px; color: var(--neon-cyan);"></i></a></h3>
                         <p style="margin-top: 15px; color: #cbd5e1;">Cisco Certified Network Associate</p>
                     </div>
                 </div>
